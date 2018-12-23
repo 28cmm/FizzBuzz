e647d38e023bef4d6ae7a9103582b354e57e7164
Yilei Huang <hyl@Yileis-MacBook-Pro.local>
2018-12-22 20:23:45 -0800

    first

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -2,14 +2,32 @@
 //  main.c
 //  FizzBuzz
 //
-//  Created by Yilei Huang on 2018-12-22.
+//  Created by Joshua Fang on 2018-12-22.
 //  Copyright © 2018 Yilei Huang. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int array[100];
+    int k = 1;
+    for(int i =0; i <100;i++){
+        array[i] = k;
+        k++;
+    }
+    
+    
+    for(int i =0; i < 100;i++){
+        if(array[i]%3 ==0 && array[i]%5==0){
+            printf("FizzBuzz\n");
+        }else if(array[i]%3 ==0){
+            printf("Fizz\n");
+        }else if(array[i]%5 ==0){
+            printf("Buzz\n");
+        }else {
+            printf("%d\n",array[i]);
+        }
+    }
+    
 }
+
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  FizzBuzz
+//
+//  Created by Yilei Huang on 2018-12-22.
+//  Copyright © 2018 Yilei Huang. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+#include <stdio.h>
+
+#endif /* test_h */

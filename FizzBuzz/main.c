ab2295e58438d508714f8d9fa569338bc91f14ad
Yilei Huang <hyl@yileis-macbook-pro.local>
2018-12-22 17:09:56 -0800

    Initial Commit

---
+++ FizzBuzz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FizzBuzz
+//
+//  Created by Yilei Huang on 2018-12-22.
+//  Copyright © 2018 Yilei Huang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
